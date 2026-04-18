39b8e3d82cd426d42240c1f2494bd4dac3e8ddea
A1-ONLINE-TET <1982chandramathan@gmail.com>
2026-04-18 23:13:51 +0530

    Update specialContentMap.js

--- PAPER 1/js/data/specialContentMap.js
+++ PAPER 1/js/data/specialContentMap.js
@@ -1,26 +1,5 @@
 export const specialContentMap = {
-  "class678": {
-    "g6_class678_4": {
-      "filename": "g6_class678_4",
-      "category": "class678",
-      "grade": "6"
-    },
-    "GRAMMER": {
-      "filename": "g6_class678_1",
-      "category": "class678",
-      "grade": "6",
-      "subject": "ENGLISH"
-    },
-    "g6_class678_1": {
-      "filename": "g6_class678_1",
-      "category": "class678",
-      "grade": "6"
-    },
-    "Untitled": {
-      "filename": "g6_class678_4",
-      "category": "class678",
-      "grade": "6"
-    },
+  
     "முக்கிய குறிப்புகள் - பகுதி 4": {
       "filename": "sample_678_notes_4",
       "category": "class678",
