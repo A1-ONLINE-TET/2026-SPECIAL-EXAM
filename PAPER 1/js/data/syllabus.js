4b0aa0e596e7a12101465b4e9b09d6adbfeb8c20
A1-ONLINE-TET <1982chandramathan@gmail.com>
2026-04-18 19:45:36 +0530

    Update syllabus.js

--- PAPER 1/js/data/syllabus.js
+++ PAPER 1/js/data/syllabus.js
@@ -3433,11 +3433,6 @@ export const syllabusData = {
                 "title": "வளிமண்டலம்",
                 "isUpdated": true,
                 "code": "soc_5_t1_l4"
-              },
-              {
-                "title": "காயத்ரி ",
-                "isUpdated": true,
-                "code": "soc_5_t1_l5"
               }
             ]
           }
