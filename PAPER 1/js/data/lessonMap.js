6d973e6c08e466a22eab7e69c94f58dd485fc938
muthukannan0510-dotcom <muthukannan0510@gmail.com>
2026-05-07 22:02:27 +0530

    Final attempt: Corrected lessonMap structure for Grade 3 Math and Psychology

--- PAPER 1/js/data/lessonMap.js
+++ PAPER 1/js/data/lessonMap.js
@@ -12,23 +12,10 @@ export const lessonMap = {
     "mat_3_t2_l4": { "local": true, "filename": "mat_3_t2_l4", "grade": "3", "title": "காலம்" },
     "mat_3_t2_l5": { "local": true, "filename": "mat_3_t2_l5", "grade": "3", "title": "தகவல் செயலாக்கம்" }
   },
-  "tamil": {
-    "tam_3_t1_l1": { "local": true, "filename": "tam_3_t1_l1", "grade": "3", "title": "தமிழ் அமுது" },
-    "tam_3_t1_l2": { "local": true, "filename": "tam_3_t1_l2", "grade": "3", "title": "செய்து மகிழ்வோம்" },
-    "tam_3_t1_l3": { "local": true, "filename": "tam_3_t1_l3", "grade": "3", "title": "கண்ணன் செய்த உதவி" }
-  },
-  "english": {
-    "eng_3_t1_l1": { "local": true, "filename": "eng_3_t1_l1", "grade": "3", "title": "Unit 1" }
-  },
-  "science": {},
-  "social": {},
   "psychology": {
     "psy_6_l1": { "local": true, "filename": "psy_6_l1", "grade": "6", "title": "கல்வி உளவியல் அறிமுகம்" },
     "psy_6_l2": { "local": true, "filename": "psy_6_l2", "grade": "6", "title": "வளர்ச்சி மற்றும் முன்னேற்றம்" },
     "psy_6_l3": { "local": true, "filename": "psy_6_l3", "grade": "6", "title": "குழந்தை மேம்பாடு" },
     "psy_6_l4": { "local": true, "filename": "psy_6_l4", "grade": "6", "title": "ஜீன் பியாஜேயின் அறிவாற்றல் வளர்ச்சி" }
-  },
-  "revision": {},
-  "mocktest": {},
-  "standard_6_7_8": {}
+  }
 };
